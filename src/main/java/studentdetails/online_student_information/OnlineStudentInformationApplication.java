93c21e8baafc7cee2c9c689bd2d84ea2ef568648
Manojkumar886 <maddymano434@gmail.com>
2024-03-05 13:40:06 +0530

    onlinestudentinformation_project creation done

---
+++ src/main/java/studentdetails/online_student_information/OnlineStudentInformationApplication.java
@@ -0,0 +1,13 @@
+package studentdetails.online_student_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudentInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStudentInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studentdetails/online_student_information/OnlineStudentInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package studentdetails.online_student_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStudentInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
